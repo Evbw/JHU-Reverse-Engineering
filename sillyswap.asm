c44d69cfab698d1b36854ac5d3382c856fc07c19
Everett <everett.bowline@gmail.com>
2026-09-02 15:20:54 -0400

    Updating swap to print messages

--- sillyswap.asm
+++ sillyswap.asm
@@ -1,22 +1,35 @@
 [BITS 32]
 
 section .data
-        msgPrompt db "VALUE_A is: %d. VALUE_B is: %d.", 0
-        msgPrompt2 db "Performing swap now.", 0
-        VALUE_A .long 1
-        VALUE_B .long 2
-        VALUE_C .long 0
+        msgPrompt db "VALUE_A is: %d. VALUE_B is: %d.", 10, 0
+        msgPrompt2 db "Performing swap now.", 10, 0
+        VALUE_A dd 1
+        VALUE_B dd 2
+        VALUE_C dd 0
 
 section .text
+global main
+extern printf
 
 main:
-    mov ecx, msgPrompt
+        push ebp
+        mov ebp, esp
+        push dword [VALUE_B]
+        push dword [VALUE_A]
+        push msgPrompt
+        call printf
+        add esp, 12
 
+        push msgPrompt2
+        call printf
+        add esp, 4
+        xor eax, eax
+        ret
 
-swap:
-    LDR r1, VALUE_C
-    LDR r2, VALUE_A
-    LDR r3, VALUE_B
-    MOV r1, r2
-    MOV r2, r3
-    MOV r3, r1
+;swap:
+;    LDR r1, VALUE_C
+;    LDR r2, VALUE_A
+;    LDR r3, VALUE_B
+;    MOV r1, r2
+;    MOV r2, r3
+;    MOV r3, r1
